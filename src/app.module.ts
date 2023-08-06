381401ac9bd5a1ee96bbd01460739747f0cf517e
Imam Sutono <imamsutono>
2023-08-06 10:00:42 +0700

    add user and comment module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm/dist';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { CommentModule } from './comment/comment.module';
 import config from 'ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config)],
+  imports: [TypeOrmModule.forRoot(config), UserModule, CommentModule],
   controllers: [AppController],
   providers: [AppService],
 })
